235d5e272cae34f082a9d390b8ceab05ada2794e
Marlena Dulęba <marlena_duleba@o2.pl>
2023-03-19 19:54:25 +0100

    Container component added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Section from './Section/Section';
 import Notification from './Notification/Notification';
+import Container from './Container/Container';
 
 export default class App extends Component {
   state = {
@@ -34,26 +35,27 @@ export default class App extends Component {
     const positivePercentage = this.countPositiveFeedbackPercentage();
 
     return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-        </Section>
-
-        <Section title="Statistics">
-      {total ? (<Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />) : (<Notification message="There is no feedback"/>)}
-            
-        
-        </Section>
-      </div>
+    
+        <Container>
+          <Section title="Please leave feedback">
+            <FeedbackOptions
+              options={Object.keys(this.state)}
+              onLeaveFeedback={this.onLeaveFeedback}
+            />
+          </Section>
+          <Section title="Statistics">
+                {total ? (<Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={total}
+                positivePercentage={positivePercentage}
+              />) : (<Notification message="There is no feedback"/>)}
+          
+          
+          </Section>
+        </Container>
+    
     );
   }
 }
