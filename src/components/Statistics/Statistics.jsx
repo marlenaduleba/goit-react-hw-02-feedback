bea538f774a995d5f0b55a4b5eb5be133a700f2c
Marlena Dulęba <marlena_duleba@o2.pl>
2023-03-19 20:34:10 +0100

    Statistics component styled

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
 
 export default class Statistics extends Component {
   static propTypes = {
@@ -14,11 +15,11 @@ export default class Statistics extends Component {
     const { good, neutral, bad, total, positivePercentage } = this.props;
     return (
       <ul>
-        <li>Good: {good}</li>
-        <li> Neutral: {neutral}</li>
-        <li> Bad: {bad}</li>
-        <li> Total: {total}</li>
-        <li>
+        <li className={`${css.item} ${css.good}`}>Good: {good}</li>
+        <li className={`${css.item} ${css.neutral}`}> Neutral: {neutral}</li>
+        <li className={`${css.item} ${css.bad}`}> Bad: {bad}</li>
+        <li className={`${css.item} ${css.total}`}> Total: {total}</li>
+        <li className={`${css.item} ${css.percent}`}>
           Positive feedback: {positivePercentage ? positivePercentage : 0} %
         </li>
       </ul>
